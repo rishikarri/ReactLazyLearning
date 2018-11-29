27acf1679ab164a852e807c6f7b532b066002301
Rishi Karri <rishi.karri@turner.com>
2018-11-29 11:41:44 -0500

    react lazy

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React from "react";
+import React, {Suspense} from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from './Components/home';
+import Vacation from './Components/vacation';
+const Index = () => <h2>Index</h2>;
 
-const Index = () => <h2>Home</h2>;
-const About = () => <h2>About</h2>;
-const Users = () => <h2>Users</h2>;
+const AppRouter = () => {
+    const LazyHome = React.lazy(() => import('./Components/home'));
 
-const AppRouter = () => (
-    <Router>
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about/">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/users/">Users</Link>
-                    </li>
-                </ul>
-            </nav>
-
-            <Route path="/" exact component={Index} />
-            <Route path="/about/" component={About} />
-            <Route path="/users/" component={Users} />
-        </div>
-    </Router>
-);
+    console.log(LazyHome, 'lazy home');
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Index</Link>
+                        </li>
+                        <li>
+                            <Link to="/about/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/users/">Vacation</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Suspense fallback={<div>loading....</div>}>
+                    <Route path="/" exact component={Index} />
+                    <Route path="/about/" component={() => <LazyHome />} />
+                    <Route path="/users/" component={Vacation} />
+                </Suspense>
+            </div>
+        </Router>
+    );
+}
 
 export default AppRouter;
---
+++ src/Components/vacation.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Vacation = () => (
+    <div>
+        Vacation
+        </div>
+);
+
+export default Vacation;
